fd8ccb11e2da7042ef5c5976a9e0f1166d6dfc09
Taylor Otey <taylorotey5@gmail.com>
2018-08-07 20:35:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(number)
+  if number % 3 or 5
+    "FizzBuzz"
   if number % 3 == 0 
     "Fizz"
   if number % 5 == 0
     "Buzz"
-  if number % 3 or 5 == 0
-    "FizzBuzz"
   end
 end
